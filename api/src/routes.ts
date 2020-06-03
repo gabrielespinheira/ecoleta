fc4b8f06206a312bd3940e51b3ee50b4b8fcb8ea
Gabriel Araujo <gabriiel66@gmail.com>
2020-06-03 00:41:40 -0300

    controllers

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,9 +1,19 @@
 import express from 'express'
 
+import PointsController from './controllers/PointsController'
+import ItemsController from './controllers/ItemsController'
+
 const routes = express.Router()
 
+const pointsController = new PointsController()
+const itemsController = new ItemsController()
+
+// status
 routes.get('/', (request, response) => {
   return response.json({ status: true })
 })
+
+routes.get('/items', itemsController.index)
+routes.post('/points', pointsController.store)
 
 export default routes
